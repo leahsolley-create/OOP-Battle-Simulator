72d043f63faf0ecc11265de8a1b930bb2f9cac3c
leahsolley-create <leah.solley@ascte.org>
2026-09-11 11:27:24 -0500

    Update game.py

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ def main():
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     goblin2 = Goblin("Gribble")
 
-    print(f"{goblin.name} enters the arena with {goblin.health} health.")
+    print(f"{goblin2.name} enters the arena with {goblin2.health} health.")
     print("But no hero has answered the call... yet.")
 
 
